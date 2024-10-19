e0a359ae02606adae9deb267e4b4827b63bb5285
Zxuuuu <162810784+zxuuuustupid@users.noreply.github.com>
2024-10-19 20:27:32 +0800

    Add files via upload

---
+++ Untitled3.m
@@ -0,0 +1,107 @@
+data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M0_G3_LA1_RA0\Sample_1\data_motor.csv");
+data=data(:,6)
+name='TYPE713_motor_CH6'
+path='CWT-1000\motor\test\anomaly\anomalyTYPE713\anomaly'
+data=data(1:4096)
+% Untitled2(data,path,name)
+
+% %%  sample generation
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\数据生成\数据生成\out-data\out-data\TYPE0_leftaxlebox_CH17_out_gho_1.csv");
+% data=data(:,1)
+% name='shotlib_leftaxlebox_gho_1'
+% path='CWT-1000\leftaxlebox\train\anomaly'
+% Untitled2(data,path,name)
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\数据生成\数据生成\out-data\out-data\TYPE0_leftaxlebox_CH17_out_gho_1.csv");
+% data=data(:,2)
+% name='shotlib_leftaxlebox_gho_1_SECOND'
+% path='CWT-1000\leftaxlebox\train\anomaly'
+% Untitled2(data,path,name)
+% 
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\数据生成\数据生成\out-data\out-data\TYPE0_leftaxlebox_CH17_out_gho_2.csv");
+% data=data(:,1)
+% name='shotlib_leftaxlebox_gho_2'
+% path='CWT-1000\leftaxlebox\train\anomaly'
+% Untitled2(data,path,name)
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\数据生成\数据生成\out-data\out-data\TYPE0_leftaxlebox_CH17_out_gho_3.csv");
+% data=data(:,1)
+% name='shotlib_leftaxlebox_gho_3'
+% path='CWT-1000\leftaxlebox\train\anomaly'
+% Untitled2(data,path,name)
+
+
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M0_G4+G5_LA0_RA0\Sample_1\data_gearbox.csv");
+% data=data(:,6)
+% name='TYPE89_gearbox_CH15'
+% path='CWT-1000\gearbox\test\anomaly\anomalyTYPE89\anomaly'
+% Untitled2(data,path,name)
+
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M0_G0_LA1+LA2+LA3+LA4_RA0\Sample_1\data_leftaxlebox.csv");
+% data=data(:,2)
+% name='TYPE13141516_leftaxlebox_CH17'
+% path='CWT-1000\leftaxlebox\test\anomaly\anomalyTYPE13141516\anomaly'
+% Untitled2(data,path,name)
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M1_G0_LA1_RA0\Sample_1\data_gearbox.csv");
+% data=data(:,6)
+% name='TYPE113_gearbox_CH15'
+% path='CWT-1000\gearbox\test\anomaly\anomalyTYPE113\anomaly'
+% Untitled2(data,path,name)
+% 
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M1_G0_LA1_RA0\Sample_1\data_rightaxlebox.csv");
+% data=data(:,2)
+% name='TYPE113_rightaxlebox_CH20'
+% path='CWT-1000\rightaxlebox\test\anomaly\anomalyTYPE113\anomaly'
+% Untitled2(data,path,name)
+% 
+% 
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M1_G0_LA1_RA1\Sample_1\data_gearbox.csv");
+% data=data(:,6)
+% name='TYPE11317_gearbox_CH15'
+% path='CWT-1000\gearbox\test\anomaly\anomalyTYPE11317\anomaly'
+% Untitled2(data,path,name)
+% 
+% 
+% clear all
+% clc
+% 
+% data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Final_Stage\Training\M0_G3_LA1_RA0\Sample_1\data_rightaxlebox.csv");
+% data=data(:,2)
+% name='TYPE713_rightaxlebox_CH20'
+% path='CWT-1000\rightaxlebox\test\anomaly\anomalyTYPE713\anomaly'
+% Untitled2(data,path,name)
+
+
+
+
---
+++ csv2mat.m
@@ -0,0 +1,5 @@
+
+data=xlsread("D:\science\PHM\PHM比赛数据集\Data_Pre Stage\Data_Pre Stage\Training data\TYPE0\Sample1\data_leftaxlebox.csv");
+data=data(:,2)
+save('TYPE0_leftaxlebox_CH17_in.mat','data');
+
